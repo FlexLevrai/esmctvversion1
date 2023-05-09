49937584d05e39c4b7ba2ff8f33da2ffba28f893
Vlax <vlax.dev@gmail.com>
2023-05-09 08:12:33 +0000

    050923_1

--- esmcwebtv/src/app/app-routing.module.ts
+++ esmcwebtv/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './website/home/home.component';
+import { EmissionComponent } from './website/emission/emission.component';
+import { VideoComponent } from './website/video/video.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  // {path: 'emission', component: ProgrammeComponent},
-  // {path: 'video/:id', component: PageVideoComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'emission', component: EmissionComponent },
+  { path: 'video/:id', component: VideoComponent },
   // {path: 'live', component: LiveComponent}
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
